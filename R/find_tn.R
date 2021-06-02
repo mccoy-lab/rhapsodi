afdb059bc46d9806cf710b6a1c7d1f0b5238ba63
Kate Weaver <kweave23@jhu.edu>
2021-06-02 12:19:00 -0400

    rename sim scripts with sim_

--- R/find_tn.R
+++
@@ -1,4 +0,0 @@
-find_tn <- function(truth_dt_na, pred_dt_na){
-  tn <- nrow(merge(truth_dt_na, pred_dt_na, by="gam")) #both truth and predicted return NA for a given gamete meaning that they both agree that gamete has no recombination events
-  return (tn)
-}
