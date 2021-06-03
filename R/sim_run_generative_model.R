522469a6262fc8f2632763376be25cdcdc7eaf05
Kate Weaver <kweave23@jhu.edu>
2021-06-03 11:18:37 -0400

    removed stringr dependency and tested named list

--- R/sim_run_generative_model.R
+++ R/sim_run_generative_model.R
@@ -4,19 +4,32 @@
 sim_run_generative_model <- function(){
   #get argument inputs
   #input_cov a bool
+  input_cov <- TRUE
   ##coverage a float
+  coverage <- 0.693
   #input_mgr a bool
+  input_mgr <- FALSE
   ##missing_genotype_rate a float
   #random_seed an int
+  random_seed <- 42
   #num_gametes an int
+  num_gametes <- 1000
   #recomb_lambda a float
+  recomb_lambda <- 1
   #num_snps an int
+  num_snps <- 30000
   #add_de_novo_mut a bool
+  add_de_novo_mut <- TRUE
   ##de_novo_lambda an int
+  de_novo_lambda <- 5
   ##de_novo_alpha a float
+  de_novo_alpha <- 7.5
   ##de_novo_beta a float
+  de_novo_beta <- 10
   #add_seq_error a bool
+  add_seq_error <- TRUE
   ##seqError_add a float
+  seqError_add <- 0.005
   
   if (input_cov){
     missing_genotype_rate <- sim_find_mgr_from_cov(coverage)
@@ -38,7 +51,7 @@ sim_run_generative_model <- function(){
   crossover_indices <- sapply(sim_gam, "[[", 1)
   names(crossover_indices) <- paste0(rep("gam", num_gametes), 1:num_gametes, "_")
   unlist_ci <- unlist(crossover_indices, use.names=TRUE)
-  tci_dt <- data.table(gam=str_split(names(unlist_ci), "_", simplify=TRUE)[,1], start =(unlist_ci-1), end=(unlist_ci))
+  tci_dt <- data.table(gam=sapply(strsplit(names(unlist_ci), "_"), `[`, 1), start =(unlist_ci-1), end=(unlist_ci))
   
   gam_mat <- sapply(sim_gam, "[[", 2)
   if (missing_genotype_rate > 0.5){
@@ -48,9 +61,7 @@ sim_run_generative_model <- function(){
   }
   
   if (add_de_novo_mut){
-    dnm_out <- sim_add_de_novo_mut(de_novo_lambda, de_novo_alppha, de_novo_beta, num_snps, num_gametes, gam_haps, gam_mat, gam_mat_with_na, donor_haps, unlist_ci, missing_genotype_rate)
-    #do I have to do anything fancy to get objects from this, or is it ok since it's a named list?
-    
+    dnm_out <- sim_add_de_novo_mut(de_novo_lambda, de_novo_alpha, de_novo_beta, num_snps, num_gametes, gam_haps, gam_mat, gam_mat_with_na, donor_haps, unlist_ci, missing_genotype_rate)
     num_snps <- dnm_out$num_snps
     
     gam_mat_with_na <- dnm_out$gam_mat_with_na
@@ -58,7 +69,9 @@ sim_run_generative_model <- function(){
     gam_mat <- dnm_out$gam_mat
     
     unlist_ci <- dnm_out$unlist_ci
-    tci_dt <- data.table(gam=str_split(names(unlist_ci), "_", simplify=TRUE)[,1], start =(unlist_ci-1), end=(unlist_ci))
+    tci_dt <- data.table(gam=sapply(strsplit(names(unlist_ci), "_"), `[`, 1), start =(unlist_ci-1), end=(unlist_ci))
+    
+    new_dnm_rows <- dnm_out$new_rows
   }
   if (add_seq_error){
     gam_mat_with_na <- sim_add_seq_error(num_snps, num_gametes, seqError_add, gam_mat_with_na)
@@ -67,4 +80,5 @@ sim_run_generative_model <- function(){
   gam_full_df <- data.frame(pseudo_pos = 1:nrow(gam_mat), gam_mat) %>% `colnames<-`(c("positions", paste0("gam", 1:num_gametes, "_")))
   
   #filtering?
+  
 }
