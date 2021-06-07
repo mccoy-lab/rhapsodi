4a1276947f365becc94e0301eca9da1cb94a5c3a
Kate Weaver <kweave23@jhu.edu>
2021-06-07 17:12:29 -0400

    unused outdir arg and needs docs

---
+++ R/rhapsodi_autorun.R
@@ -0,0 +1,12 @@
+#'
+#'
+#'
+rhapsodi_autorun <- function(threads=2, sampleName="sampleT", chrom = "chrT", outdir = "tmp/", seqError_model = 0.005, avg_recomb_model = 1, 
+                             window_length=3000, overlap_denom = 2, mcstop=TRUE, stringent_stitch=TRUE, stitch_new_min=0.5,
+                             smooth_imputed_genotypes=FALSE, smooth_crossovers=TRUE){
+  ##input data --> dt, positions
+  complete_haplotypes <- impute_donor_haplotypes(dt, positions, window_length = window_length, overlap_denom = overlap_denom, threads=threads, mcstop=mcstop, stringent_stitch=stringent_stitch, stitch_new_min = stitch_new_min)
+  filled_gametes <- fill_gametes(dt, complete_haplotypes, sequencing_error = seqError_model, avg_recomb = avg_recomb_model, threads = threads)
+  rhapsodi_out <- report_gametes(smooth_crossovers, smooth_imputed_genotypes, complete_haplotypes, dt, filled_gametes, sampleName, chrom)
+  return (rhapsodi_out)
+}
