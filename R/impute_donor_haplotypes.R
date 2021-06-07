91bc8c6228446ee3ae68ecc972d6e1fd91eee0b2
Kate Weaver <kweave23@jhu.edu>
2021-06-07 16:00:28 -0400

    docs might need more update but calls stitch_haplotypes function

--- R/impute_donor_haplotypes.R
+++ R/impute_donor_haplotypes.R
@@ -11,13 +11,15 @@
 #' @param overlap_denom User-input value for denominator in calculation of overlap (default = 2)
 #' @param threads User-input value for calling `pbmclapply` (default = 2)
 #' @param mcstop User-input value for continuing phasing even if mean_concordance isn't within advised bounds (default = FALSE)
+#' @param stringent_stitch Boolean, whether or not to use the original bounds (0.1, 0.9 cutoff)
+#' @param stitch_new_min Numeric/float, if `stringent_stitch` == FALSE, the new minimum value to use as a cutoff. If concordance is below this value, we assume the windows are opposite haplotypes, if greater than or equal, we assume the same haplotypes
 #' 
 #' @return complete_haplotypes phased haplotypes as a tibble with column names index, pos (for SNP positions), h1 (haplotype 1), & h2 (haplotype 2)
 #' 
 #' @example 
 #' R code here showing my function works 
 #' 
-impute_donor_haplotypes <- function(dt, positions, window_length=3000, overlap_denom=2, threads=2, mcstop=TRUE){
+impute_donor_haplotypes <- function(dt, positions, window_length=3000, overlap_denom=2, threads=2, mcstop=TRUE, stringent_stitch=TRUE, stitch_new_min=0.5){
   #Find overlapping windows
   windows <- split_with_overlap(rank(positions), window_length, overlap_denom)
   #Infer the haplotypes within the overlapping windows (a window at a time)
@@ -25,31 +27,6 @@ impute_donor_haplotypes <- function(dt, positions, window_length=3000, overlap_d
                                     function(x) reconstruct_hap(dt, positions, windows[[x]]),
                                     mc.cores = getOption("mc.cores", threads))
   #Stitch together the haplotypes
-  initial_haplotype <- inferred_haplotypes[[1]]
-  for (hap_window in 1:length(windows)){
-    olap_haps <- merge(initial_haplotype, inferred_haplotypes[[hap_window]], by = "index")
-    olap_haps_complete <- merge(initial_haplotype, inferred_haplotypes[[hap_window]], by = "index", all = TRUE)
-    mean_concordance <- mean(olap_haps$h1.x == olap_haps$h1.y, na.rm=TRUE)
-    if (mean_concordance < 0.1) {
-      olap_haps_complete$h1.y <- invert_bits(olap_haps_complete$h1.y)
-    } else if (mean_concordance < 0.9) {
-      if (mcstop){
-        stop(paste0("Haplotypes within overlapping windows are too discordant to merge with a mean concordance of ", mean_concordance, ". rhapsodi is exiting"))
-      } else{
-        message(paste0("Haplotypes within overlapping windows are too discordant for confident merging with a mean concordance of ", mean_concordance, ", but continuing."))
-      }
-    }
-    initial_haplotype <- tibble(index = olap_haps_complete$index,
-                                pos = c(olap_haps_complete[is.na(olap_haps_complete$pos.y),]$pos.x,
-                                        olap_haps_complete[!is.na(olap_haps_complete$pos.x) &
-                                                             !is.na(olap_haps_complete$pos.y),]$pos.x,
-                                        olap_haps_complete[is.na(olap_haps_complete$pos.x),]$pos.y),
-                                h1 = c(olap_haps_complete[is.na(olap_haps_complete$pos.y),]$h1.x,
-                                       olap_haps_complete[!is.na(olap_haps_complete$pos.x) &
-                                                            !is.na(olap_haps_complete$pos.y),]$h1.x,
-                                       olap_haps_complete[is.na(olap_haps_complete$pos.x),]$h1.y))
-  }
-  complete_haplotypes <- initial_haplotype %>%
-    mutate(h2 = invert_bits(h1))
+  complete_haplotypes <- stitch_haplotypes(inferred_haplotypes, windows, mcstop=mcstop, stringent_stitch=stringent_stitch, stitch_new_min=stitch_new_min)
   return(complete_haplotypes)
 }
