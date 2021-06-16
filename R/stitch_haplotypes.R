1ed94845cc3f93008c5ad024d782d103b17be813
Kate Weaver <kweave23@jhu.edu>
2021-06-16 16:50:36 -0400

    minor mod and docs update

--- R/stitch_haplotypes.R
+++ R/stitch_haplotypes.R
@@ -1,11 +1,30 @@
+#' A function to stitch overlapping windows together into the two donor haplotypes
+#' 
+#' This function stitches together the haplotypes of overlapping windows by considering the amount of shared concordance between each window
+#' Specific behavior depends on the parameters. There are two main modes by which this function operates. 
+#' The first is stringent stitching which uses two preset threshold values to determine whether or not the windows originate from the same donor
+#' The second is non-stringent stitching which uses a single user-defined threshold value to determine whether or not the windows originate from the same donor
+#' In both cases, consider there being a `different_max` threshold, where if the concordance between two windows is less than this value, we would say that the windows originate from different donors
+#' and then there being a `same_min` threshold, where if the concordance between two windows is greater than this value, we would say that the windows originate from the same donor
+#' In the first stringent stitching case, these are different values, 0.1 and 0.9 respectively. 
+#' In the second non-stringent stitching case, these are the same value, recommended and default value of 0.5, but user may change.
+#' Mean concordance is found by finding the mean number of locations that have equal values, removing NAs from consideration since majority voting may return NA for some positions
+#' The function stitches together the first haplotype and finds the second by inverting the first haplotype.
+#' 
+#' @param inferred_haplotypes a list of tibbles from calling `reconstruct_hap` within `impute_donor_haplotypes` where each tibble is the inferred haplotype sorted by position for that window 
+#' @param windows a list of lists from `split_with_overlap` within `impute_donor_haplotypes` where the number of lists equal to the number of windows and each inner list contains the SNP positions in that overlapping segment
+#' @param stringent_stitch a bool, this parameter is used to determine the threshold values used in determining whether two windows originate from the same donor. If TRUE, the preset thresholds of 0.1 and 0.9 are used.
+#' @param stitch_new_min a numeric >0, but <1; default is 0.5; this parameter is only evaluated if `stringent_stitch` is FALSE and is dually assigned as the `different_max` and `same_min` threshold values when considering the concordance between two windows and therefore which donors they originate from (same or different). 
+#' @param mcstop a bool, only considered if `stringent_stitch` is TRUE; default is TRUE; this parameter is used to determine whether phasing continues or exits if the mean concordance between two windows is between 0.1 and 0.9. If TRUE, rhapsodi exits. If FALSE, rhapsodi and phasing continues, acting as if mean concordance was greater than 0.9
 #'
-#'
-#'
+#' @return complete_haplotypes a tibble with two columns, h1 and h2, containing the inferred haplotypes from each window stitched together in a single non-overlapping segment 
+#' 
 stitch_haplotypes <- function(inferred_haplotypes, windows, mcstop=TRUE, stringent_stitch=TRUE, stitch_new_min=0.5){
   if (stringent_stitch){
     different_max = 0.1
     same_min = 0.9
   } else{
+    stopifnot(stitch_new_min < 1 & stitch_new_min > 0)
     different_max = stitch_new_min
     same_min = stitch_new_min
   }
