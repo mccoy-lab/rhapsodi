06c18b657fb314c8f3be7ad82fffe73cf9bf6810
Kate Weaver <kweave23@jhu.edu>
2021-06-18 19:17:48 -0400

    cov and mgr docs

--- R/sim_find_cov_from_mgr.R
+++ R/sim_find_cov_from_mgr.R
@@ -1,5 +1,11 @@
-#'
-#'
+#' This function computes the coverage given the desired missing genotype rate
+#' 
+#' This function computes the coverage of the simulation given the desired missing genotype rate
+#' by reversing the poisson density function and taking the negative natural log of the input missing genotype rate
+#' 
+#' @param missing_genotype_rate a numeric, if greater than 1, assumed to be a percentage and is divided by 100. 
+#' 
+#' @return coverage a numeric, the coverage of the simulation corresponding to the desired missing_genotype_rate
 #'
 sim_find_cov_from_mgr <- function(missing_genotype_rate){
   if (missing_genotype_rate > 1){ #if entered as a percentage
--- R/sim_find_mgr_from_cov.R
+++ R/sim_find_mgr_from_cov.R
@@ -1,6 +1,14 @@
-#'
-#'
-#'
+#' This function computes the missing genotype rate given the coverage
+#' 
+#' This function computes the missing genotype rate of the simulation given the desired coverage
+#' by using the poisson density function parameterized by x = 0 and lambda equal to the coverage metric
+#' to return the probability of the density function, or the missing genotype rate
+#' Note that the function checks to assure that the coverage is greater than 0
+#' 
+#' @param coverage a numeric, must be greater than 0. The desired coverage of sequencing (e.g. 0.01x, 0.1x, 1.204x, etc)
+#' 
+#' @return missing_genotype_rate a numeric, the corresponding missing genotype rate for the given coverage, < 1
+#' 
 sim_find_mgr_from_cov <- function(coverage){
   stopifnot(coverage > 0)
   message(paste0("The coverage of this simulation is: ", coverage))
