262214ffdde3d7f4287249e17588ce645e02fedf
Kate Weaver <kweave23@jhu.edu>
2021-06-19 01:43:58 -0400

    tn fn docs

--- R/sim_find_fn.R
+++ R/sim_find_fn.R
@@ -1,3 +1,19 @@
+#' This function finds the number of false negatives when comparing two sets of recombinantion breakpoints
+#'
+#' This function finds the number of false negatives when comparing by finding the locations for which nnothing 
+#' intersects true recombination locations.  
+#' When multiple predictions intersect a single truth, in the conservative approach, we consider all but one of the
+#' the intersections to be false negatives because we in essence aren't predicting a true recomb spot, or we're falsely 
+#' saying there's not a recombination spot there.  
+#' 
+#' @param truth_intersect_dt a data table from foverlaps intersecting the truth and the predicted data with columns of `gam`, `Predicted_Start`, `Predicted_End`, `True_Start`, `True_End`
+#' @param truth_dt_nona a data table of just the truth data with no NAs in `start` or `stop` columns.
+#' @param cons a bool, default is FALSE; only applicable when multiple predictions intersect a single truth. if `cons` is TRUE, we take a conservative approach and consider all but one of these multiple intersections as false negatives   
+#' @param no_truths a bool, default is FALSE, if true, that means there were only NAs in the truth information and no real recombination break points
+#' @param no_preds a bool, default is FALSE, if true, that means there wer only NAs in the prediction information and no predicted recombination break points
+#' 
+#' @return fn an integer, the number of false negatives
+#' 
 sim_find_fn <- function(truth_intersect_dt, truth_dt_nona, cons=FALSE, no_truths=FALSE, no_preds=FALSE){
   if (!no_truths & !no_preds){
     fn <- nrow(truth_intersect_dt[is.na(truth_intersect_dt$Predicted_Start),]) #number of true recombinantion spots that don't interesect any predictions
--- R/sim_find_tn.R
+++ R/sim_find_tn.R
@@ -1,3 +1,13 @@
+#' This function finds the number of true negatives when comparing two sets of recombination breakpoints
+#' 
+#' This function find the number of true negatives by finding the gametes for which both truth and prediction say there are no 
+#' recombination breakpoints (NA in start and stop columns)
+#' 
+#' @param truth_dt_na a data table for the truth or generated data in which the `start` and `stop` columns have NA, meaning no recombination breakpoints
+#' @param pred_dt_na a data table for the predicted data from rhapsodi in which the `start` and `stop` columns have NA, meaning no predicted recombination breakpoints
+#' 
+#' @return tn an integer, the number of true negatives
+#'
 sim_find_tn <- function(truth_dt_na, pred_dt_na){
   tn <- nrow(merge(truth_dt_na, pred_dt_na, by="gam")) #both truth and predicted return NA for a given gamete meaning that they both agree that gamete has no recombination events
   return (tn)
