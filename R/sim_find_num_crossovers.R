a5ef2a611a5ee490e5bc1ec1c129cb03eeb21984
Kate Weaver <kweave23@jhu.edu>
2021-06-18 23:37:08 -0400

    sim find nums docs

--- R/sim_find_num_crossovers.R
+++ R/sim_find_num_crossovers.R
@@ -1,5 +1,12 @@
-#'
-#'
+#' This function finds the number of crossovers for each gamete
+#' 
+#' This function uses a poisson distribution with mean equal to `recomb_lambda` to randomly generate the number of crossovers
+#' for each gamete for `num_gametes` gametes
+#' 
+#' @param num_gametes an integer, the number of gametes for which to generate a number of crossovers
+#' @param recomb_lambda a numeric, the average recombination rate, used as the mean for the poisson distribution
+#' 
+#' @return num_recomb_sites a vector of length `num_gametes` containing integers for the number of crossovers for each gamete
 #'
 sim_find_num_crossovers <- function(num_gametes, recomb_lambda){
   stopifnot(recomb_lambda > 0)
--- R/sim_find_num_nas.R
+++ R/sim_find_num_nas.R
@@ -1,5 +1,13 @@
+#' This function computes the number of NAs that should be used to replace genotypes given the size of the genotype matrix and the missing genotype rate
+#' 
+#' This function computes the number of NAs that are needed to achieve a certain missing genotype rate
+#' given the size of the input genotype matrix which is num_snps rows and num_gametes columns. 
+#' 
+#' @param num_gametes an integer, the number of gametes or the number of columns of the genotype matrix
+#' @param num_snps an integer, the number of snps or the number of rows of the genotype matrix 
+#' @param missing_genotype_rate a numeric, < 1, the missing genotype rate
 #'
-#'
+#' @return num_nas an integer, the product of the missing genotype rate and the number of genotypes (`num_snps` * `num_gametes`)
 #'
 sim_find_num_nas <- function(num_gametes, num_snps, missing_genotype_rate){
   num_genotypes <- num_gametes * num_snps
