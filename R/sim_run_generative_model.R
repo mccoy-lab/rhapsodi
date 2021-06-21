3913954aedfd2d6ba5ac8ef40dafd171e31e63fb
Kate Weaver <kweave23@jhu.edu>
2021-06-21 12:34:45 -0400

    one more doc done two left

--- R/sim_run_generative_model.R
+++ R/sim_run_generative_model.R
@@ -1,7 +1,16 @@
+#' This function runs the generative model to simulate input sparse gamete data for rhapsodi
+#' 
+#' This function runs the generative model to simulate input sparse gamete data for rhapsodi. In addition,
+#' to returning the sparse gamete data, the function also returns the fully known generated gamete data,
+#' the diploid donor phased haplotypes, and the true recombination break points for each gamete.
+#' The following variables of the simulation can all be controlled:
+#' the number of gametes, the number of SNPs, the sequencing coverage (or missing genotype rate), 
+#' the average recombination rate, whether to simulate sequencing error, the sequencing error rate to use,
+#' whether to add de novo mutations, values parameterizing how many de novo mutations there are and how many gametes are affected by the de novo mutations,
+#' and the random seed for reproducibility
 #'
-#'
-#' @param num_gametes an integer
-#' @param num_snps an integer
+#' @param num_gametes an integer, the number of gametes, or the number of columns for the sparse gamete data you want generated
+#' @param num_snps an integer, the number of SNPs, or the number of rows for the sparse gamete data you want generated. Note: not all of these will be heterozygous due to the coverage and therefore this number won't necessarily equal the number of SNPs following filtering at the end of the generation 
 #' @param coverage a numeric, input if input_cov is TRUE, suggested NULL otherwise
 #' @param recomb_lambda a numeric, the average rate of recombination expected for the simulation
 #' @param random_seed an integer, the random seed which will be set for the simulation, default=42
@@ -11,11 +20,15 @@
 #' @param add_seq_error a logical, TRUE if you want to add sequencing error to the generated data, default=TRUE
 #' @param seqError_add a numeric, the sequencing error rate if adding sequencing error to the generated data, default=0.005
 #' @param add_de_novo_mut a logical, TRUE if you want to add de novo mutations to the generated data, default=FALSE
-#' @param de_novo_lambda an integer, default=5
-#' @param de_novo_alpha a numeric, default=7.5
-#' @param de_novo_beta a numeric, default=10
+#' @param de_novo_lambda an integer, default=5, parameterizes a poisson distribution to find the number of de novo mutations (DNM) total
+#' @param de_novo_alpha a numeric, default=7.5, shape parameter for a gamma distribution to find the number of gametes affected per DNM
+#' @param de_novo_beta a numeric, default=10, scale parameter for a gamma distribution to find the number of gametes affected per DNM
 #'
+#' @return generated_data a named list returning the generated input and full truth data, specifically `gam_na` for the sparse rhapsodi input, `gam_full` for the fully known gamete data input equivalent, `recomb_spots` for the true recombination spots for each gamete, and `donor_haps` for the diploid donor phased haplotypes
 #'
+#' @import tidyverse
+#'
+#' @export
 sim_run_generative_model <- function(num_gametes, num_snps, coverage, 
                                      recomb_lambda, random_seed=42, 
                                      input_cov=TRUE, input_mgr=FALSE, missing_genotype_rate=NULL,
